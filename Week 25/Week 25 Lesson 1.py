0822f2fcb2e69f48b5537b9df8c78a5b47312835
Jake Brotherton <thesnakejake2006@gmail.com>
2021-04-20 14:55:59 +0100

    20/04/2021

--- Week 25/Week 25 Lesson 1.py
+++ Week 25/Week 25 Lesson 1.py
@@ -1,3 +1,13 @@
-print("What is your name?")
-firstname = input()
-print("Hello,", firstname)
+import random
+import time
+
+# Prints a welcome message
+print("Hello I am Bot! :3")
+# Waits 1 second by importing time
+time.sleep(1)
+# Asks for your firstname and surname and combines them to get your full name
+firstname = input("What is your first name? ")
+lastname = input("And your surname? ")
+fullname = firstname + " " + lastname
+# Prints a final statement using the full name
+print("Hello", fullname + ",", "It is very nice to meet you!")
