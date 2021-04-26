3e3c6479c2fd3f3fac2794b411f211adb3b9e1b9
Jake Brotherton <thesnakejake2006@gmail.com>
2021-04-26 16:19:06 +0100

    26/04/21

---
+++ Homework/Homework - If Statements.py
@@ -0,0 +1,26 @@
+# Question 2
+# ---------------------------------------------------------------------------------------------------------------
+
+needed = 20
+inPiggyBank = 10.50
+extra = float(input("How much extra money do you have? "))
+total = inPiggyBank + extra
+
+if total >= needed:
+	print("You have enough money Billy Biggins :D")
+else:
+	print("You need more money Billy Biggins D:")
+
+# ---------------------------------------------------------------------------------------------------------------
+
+# Question 3
+# ---------------------------------------------------------------------------------------------------------------
+
+choice = input("Choose an (I)nsult or a (C)ompliment: ")
+
+if choice == "I" or choice == "i":
+	print("Your nose is so small I can barely see it >:)")
+elif choice == "C" or choice == "c":
+	print("You have a nice shirt on today :)")
+
+# ---------------------------------------------------------------------------------------------------------------
