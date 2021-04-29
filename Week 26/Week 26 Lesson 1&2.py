0d8763be2e6c88c18da2a16e3793bacfdcf45395
Jake Brotherton <thesnakejake2006@gmail.com>
2021-04-29 14:04:53 +0100

    27/04/21

--- Week 26/Week 26 Lesson 1&2.py
+++ Week 26/Week 26 Lesson 1&2.py
@@ -9,6 +9,15 @@ print("You have £" + str(money))
 # Lesson 2
 # ---------------------------------------------------------------------------------------------------------------
 
-print("TO BE CHANGED")
+username = float(input("What is your username? "))
+network = float(input("What network are you using? "))
+numberofmins = int(input("How many minutes have you used this month? "))
+numberoftexts = int(input("How many texts have you sent in the past month? "))
+numberofmins = numberofmins * 0.10
+numberoftexts = numberoftexts * 0.05
+cost = numberofmins + numberoftexts
+vat = cost/5
+costwithvat = cost + vat
+print(costwithvat)
 
 # ---------------------------------------------------------------------------------------------------------------
