b18c9165cb7f445c6db1f442bee5fd52c1d42213
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-04 14:59:10 +0100

    04/05/21

--- Week 27/Week 27 Lesson 1&2.py
+++ Week 27/Week 27 Lesson 1&2.py
@@ -1,11 +1,31 @@
 # Lesson 1
 # ---------------------------------------------------------------------------------------------------------------
 
+# Task 1
 speed = int(input("What speed are you going at? "))
 
 if speed >= 70:
     print("You owe the caught money for speeding! >:(")
 else:
     print("Good job, make sure to stay within the speed limit! :)")
+
+# Task 2
+grade = int(input("Enter your grade: "))
+
+if grade >= 80:
+    print("Distinction")
+elif grade >= 70:
+    print("Merit")
+elif grade >= 60:
+    print("Pass")
+else:
+    print("Fail")
+
+# ---------------------------------------------------------------------------------------------------------------
+
+# Lesson 2
+# ---------------------------------------------------------------------------------------------------------------
+
+# TO BE CONTINUED
 
 # ---------------------------------------------------------------------------------------------------------------
