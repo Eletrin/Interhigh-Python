716072907e1d1f02a7bb96c1ca25271c79d36f5b
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-04 14:43:08 +0100

    04/05/21

---
+++ Week 27/Week 27 Lesson 1&2.py
@@ -0,0 +1,11 @@
+# Lesson 1
+# ---------------------------------------------------------------------------------------------------------------
+
+speed = int(input("What speed are you going at? "))
+
+if speed >= 70:
+    print("You owe the caught money for speeding! >:(")
+else:
+    print("Good job, make sure to stay within the speed limit! :)")
+
+# ---------------------------------------------------------------------------------------------------------------
