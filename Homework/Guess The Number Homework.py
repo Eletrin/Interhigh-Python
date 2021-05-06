688be6b7bb6a0764df6baa99f285938d798ba62e
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-06 14:06:52 +0100

    06/05/21

--- Homework/Guess The Number Homework.py
+++ Homework/Guess The Number Homework.py
@@ -1,15 +1,17 @@
 import time
+import random
 
 print("I am thinking of a number between 1 and 10")
 time.sleep(2)
-number = int(input("Try and guess my number: "))
+guess = int(input("Try and guess my number: "))
+number = random.randint(1, 10)
 
-while number != 6:
+while guess != number:
     print("That is not my number")
     time.sleep(1)
     number = int(input("Try and guess my number: "))
 
-if number == 6:
+if guess == number:
     print("Yay you guessed my number!")
 else:
     print("Error Occurred :P")
