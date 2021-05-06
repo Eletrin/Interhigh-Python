331fc255f2aaa3fe999374fc35ddeb2ec04593a5
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-06 14:10:29 +0100

    06/05/21

--- Week 27/Week 27 Lesson 1&2.py
+++ Week 27/Week 27 Lesson 1&2.py
@@ -1,3 +1,7 @@
+# Imports
+import time
+import random
+
 # Lesson 1
 # ---------------------------------------------------------------------------------------------------------------
 
@@ -35,6 +39,20 @@ else:
     print("Sorry the answer was 4")
 
 # Task 2
+print("I am thinking of a number between 1 and 10")
+time.sleep(2)
+guess = int(input("Try and guess my number: "))
+number = random.randint(1, 10)
+
+while guess != number:
+    print("That is not my number")
+    time.sleep(1)
+    number = int(input("Try and guess my number: "))
+
+if guess == number:
+    print("Yay you guessed my number!")
+else:
+    print("Error Occurred :P")
 
 
 # ---------------------------------------------------------------------------------------------------------------
