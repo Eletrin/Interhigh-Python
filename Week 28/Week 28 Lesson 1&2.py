3162c17c8cb899245a402cdd5bffc5012b48e2b7
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-11 14:39:53 +0100

    11/05/21

--- Week 28/Week 28 Lesson 1&2.py
+++ Week 28/Week 28 Lesson 1&2.py
@@ -1,10 +1,47 @@
 # Lesson 1
 # ---------------------------------------------------------------------------------------------------------------
 
+# Task 1
 answer = input("What is the capital of France? ")
 answer = answer.title()
 
 while answer != "Paris":
+    answer = input("Incorrect, try again: ")
+    answer = answer.title()
+
+print("Correct! Well done.")
+
+answer = input("What comes after A in the alphabet? ")
+answer = answer.title()
+
+while answer != "B":
+    answer = input("Incorrect, try again: ")
+    answer = answer.title()
+
+print("Correct! Well done.")
+
+answer = input("What's 9+10? ")
+answer = answer.title()
+
+while answer != "21":
+    answer = input("Incorrect, try again: ")
+    answer = answer.title()
+
+print("Ya Stoopid. :P")
+
+answer = input("What is 4x9? ")
+answer = answer.title()
+
+while answer != "36":
+    answer = input("Incorrect, try again: ")
+    answer = answer.title()
+
+print("Correct! Well done.")
+
+answer = input("When did WWII end? ")
+answer = answer.title()
+
+while answer != "1945":
     answer = input("Incorrect, try again: ")
     answer = answer.title()
 
