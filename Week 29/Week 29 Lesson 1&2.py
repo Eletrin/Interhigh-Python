2f77844759b9b38d2ade0c1a47f30a6ad87b7960
Jake Brotherton <thesnakejake2006@gmail.com>
2021-05-20 14:11:11 +0100

    20/05/21

--- Week 29/Week 29 Lesson 1&2.py
+++ Week 29/Week 29 Lesson 1&2.py
@@ -1,3 +1,6 @@
+# Imports
+import random
+
 # Lesson 1
 # ---------------------------------------------------------------------------------------------------------------
 
@@ -14,6 +17,37 @@ while guess != number:
 # Lesson 2
 # ---------------------------------------------------------------------------------------------------------------
 
-# To Be Continued
+# Task 1
+number = random.randint(1, 10)
+counter = 1
+
+name = input("Hello and welcome to my game! May I get your name? ")
+
+for x in range(1, 6):
+    guess = int(input('Please enter a guess between 1 and 10: '))
+    if guess == number:
+        print('Well done', name + ", It took you", counter, "tries.")
+        break
+    else:
+        print('Hard luck')
+        counter = counter + 1
+
+# Task 2
+name = input("May I please get your name? ")
+
+for x in range(1, 7):
+    print(name)
+
+# Task 3
+no1 = float(input("1st number: "))
+no2 = float(input("2nd number: "))
+no3 = float(input("3rd number: "))
+no4 = float(input("4th number: "))
+no5 = float(input("5th number: "))
+
+fullnumber = no1 + no2 + no3 + no4 + no5
+finalnumber = fullnumber / 5
+
+print("The average of all of those numbers you gave me are", finalnumber)
 
 # ---------------------------------------------------------------------------------------------------------------
