4d256bb7550a5ec1a2e4f8ad72d7f28cf14cb0f0
Jake Brotherton <thesnakejake2006@gmail.com>
2021-06-22 14:54:52 +0100

    22/06/21

---
+++ Week 33/Week 33 Lesson 1&2.py
@@ -0,0 +1,25 @@
+# Lesson 1
+# ---------------------------------------------------------------------------------------------------------------
+
+heroes = ["Batman", "Wonder Woman", "Superman", "Spiderman"]
+
+print("Current Pilot:", heroes[0])
+print("Current Co-Pilot:", heroes[1])
+heroes[2] = "Hit Girl"
+heroes.append("Iron Man")
+heroes.append("Doctor Strange")
+print(heroes)
+
+number = int(input("Which hero do you want to replace? Choose a number from 1-6!"))
+number = number-1
+heroes[number] = input("What hero do you want to replace them with: ")
+print(heroes)
+
+# ---------------------------------------------------------------------------------------------------------------
+
+# Lesson 2
+# ---------------------------------------------------------------------------------------------------------------
+
+# TO BE CONTINUED
+
+# ---------------------------------------------------------------------------------------------------------------
